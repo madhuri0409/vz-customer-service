0eefd45f60f4bf86def410bfbc3149d4738ba4ed
madhuri0409 <44791342+madhuri0409@users.noreply.github.com>
2020-06-01 00:03:28 -0500

    Eureka Registry Changes

--- src/main/java/com/verizon/customer/VzCustomerServiceApplication.java
+++ src/main/java/com/verizon/customer/VzCustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.verizon.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VzCustomerServiceApplication {
 
 	public static void main(String[] args) {
