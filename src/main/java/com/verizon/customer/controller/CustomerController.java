f4847a4af31e4b8d277456c8411a258c6eb89637
Ganga Madhuri Maddala <gangamadhuri.m@ad.infosys.com>
2020-06-17 23:04:23 -0500

    Updated Customer Service

--- src/main/java/com/verizon/customer/controller/CustomerController.java
+++ src/main/java/com/verizon/customer/controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.verizon.customer.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +14,13 @@ import com.verizon.customer.entity.CustomerAddress;
 import com.verizon.customer.model.CustomerDetails;
 import com.verizon.customer.service.CustomerService;
 
+
+
 @RestController
 public class CustomerController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
+	
 	
 	@Autowired
 	private CustomerService customerService;
@@ -30,6 +37,7 @@ public class CustomerController {
 	
 	@GetMapping("/customerById")
 	public CustomerDetails getCustomerById(@RequestParam(value = "id") Integer id) {
+		LOGGER.info("Calling Customer Service for customer id :  {}",id);
 		return customerService.getCustomerDetailsById(id);
 	}
 }
--- src/main/java/com/verizon/customer/service/CustomerService.java
+++ src/main/java/com/verizon/customer/service/CustomerService.java
@@ -3,6 +3,8 @@ package com.verizon.customer.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,26 +16,29 @@ import com.verizon.customer.repo.CustomerRepository;
 
 @Service
 public class CustomerService {
-	 
+
+	Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
 	@Autowired
 	private CustomerAddressRepository customerAddressRepository;
-	
+
 	@Autowired
 	private CustomerRepository customerRepository;
-	
-	public List<Customer> getAllCustomers(){
-		return customerRepository.findAll();	
+
+	public List<Customer> getAllCustomers() {
+		return customerRepository.findAll();
 	}
-	
-	public List<CustomerAddress> getAllCustomersAddress(){
+
+	public List<CustomerAddress> getAllCustomersAddress() {
 		return customerAddressRepository.findAll();
 	}
-	
+
 	public CustomerDetails getCustomerDetailsById(Integer customerId) {
+		LOGGER.info("In Customer Service : Fetching Customer Details Started for Customer Id {}", customerId);
 		Optional<Customer> customer = customerRepository.findById(customerId);
 		Optional<CustomerAddress> customerAddress = customerAddressRepository.findFirstByCustomerId(customerId);
 		CustomerDetails customerDetails = null;
-		if(customer.isPresent() && customerAddress.isPresent()) {
+		if (customer.isPresent() && customerAddress.isPresent()) {
 			customerDetails = new CustomerDetails();
 			Customer customerData = customer.get();
 			CustomerAddress customerAddressData = customerAddress.get();
@@ -45,6 +50,9 @@ public class CustomerService {
 			customerDetails.setState(customerAddressData.getState());
 			customerDetails.setZipcode(customerAddressData.getZipcode());
 		}
+		LOGGER.info(
+				"In Customer Service : Fetching Customer Details Complete for Customer Id {} : Customer Details : {}",
+				customerId, customerDetails);
 		return customerDetails;
 	}
 }
