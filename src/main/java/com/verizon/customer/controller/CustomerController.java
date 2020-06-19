cb3bb6818de9772a714d4360ef370588c4dc9ce4
Ganga Madhuri Maddala <gangamadhuri.m@ad.infosys.com>
2020-06-19 01:04:26 -0500

    Updated Kafka

--- src/main/java/com/verizon/customer/controller/CustomerController.java
+++ src/main/java/com/verizon/customer/controller/CustomerController.java
@@ -5,14 +5,18 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.verizon.customer.entity.Customer;
 import com.verizon.customer.entity.CustomerAddress;
 import com.verizon.customer.model.CustomerDetails;
 import com.verizon.customer.service.CustomerService;
+
+import brave.Tracer;
 
 
 
@@ -24,6 +28,15 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${kafka.service.uri}")
+	private String kafkaServiceUri;
+	
+	@Autowired
+	Tracer tracer;
 	
 	@GetMapping("/customers")
 	public List<Customer> getCustomers(){
@@ -37,7 +50,11 @@ public class CustomerController {
 	
 	@GetMapping("/customerById")
 	public CustomerDetails getCustomerById(@RequestParam(value = "id") Integer id) {
+		Long currentTime = System.currentTimeMillis();
 		LOGGER.info("Calling Customer Service for customer id :  {}",id);
-		return customerService.getCustomerDetailsById(id);
+		CustomerDetails customerdetails = customerService.getCustomerDetailsById(id);
+		String message = String.format("%d | %s | %s | %s | %s | %s", currentTime, "CustomerService", tracer.currentSpan().context().spanIdString(), tracer.currentSpan().context().traceIdString(), id, customerdetails);
+		restTemplate.getForObject(kafkaServiceUri + "=" + message, String.class);
+		return customerdetails;
 	}
 }
